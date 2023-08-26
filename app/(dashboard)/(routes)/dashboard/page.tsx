83730a4e5188d0b7b225fbad91c1f6004f4313e5
ieatn <ieatnubcake@gmail.com>
2023-08-26 14:22:19 -0700

    integrate clerk auth, env file w/ clerk keys

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,6 @@
+import { Button } from "@/components/ui/button"
+export default function DashboardPage() {
+  return (
+    <Button variant='destructive' size='lg'>dashboard page protected</Button>
+  )
+}
